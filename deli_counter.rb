1dd58ab5a3d5240d89f06003da098b175d5a75f3
Jamal Gelin <gelin.jamal@gmail.com>
2018-09-30 19:24:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,25 +1,24 @@
 # Write your code here.
-katz_deli = []
-
 def line(katz_deli)
-  if deli size == 0
+  line_array = []
+  if katz_deli.length == 0
     puts "The line is currently empty."
-     array.each_with_index {|name, i| line_message << " #{i+1}. #{name}"}
   else
-    puts "Welcome #{name}. You are #{number} in line."
+    katz_deli.each.with_index(1) do |value, index|
+      line_array.push("#{index}. #{value}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
   end
 end
-
-def take_a_number(katz_deli, customername)
-  katz_deli = []
-  aarray.push("#{customername}")
-  puts "Welcome #{customername}, you are number #{array.size} in line."
+ def take_a_number(katz_deli, new_person)
+  katz_deli << new_person
+  puts "Welcome, #{new_person}. You are number #{katz_deli.length} in line."
 end
-
-def now_serving(katz_deli)
-  if array.size > 0
-  puts "Currently serving #{array.size + 1}."
-else
-  puts "There is nobody waiting to be served."
- end
+ def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
 end
